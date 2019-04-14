a1d65f97da864c9e16d08ce8c5130acdaf03d97f
Trettende <compdolblu@gmail.com>
2019-04-14 19:41:46 +0300

    chapter32 commit

--- app/src/main/java/training/bignerdranch/sunset/SunsetActivity.java
+++ app/src/main/java/training/bignerdranch/sunset/SunsetActivity.java
@@ -1,13 +1,13 @@
 package training.bignerdranch.sunset;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SunsetActivity extends AppCompatActivity {
-
+public class SunsetActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_sunset);
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
     }
+
 }
